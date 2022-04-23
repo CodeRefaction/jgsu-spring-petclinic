5ea80673dcc122a7a7b7c68a2d247a19e7887da5
Stefan Bockes (TI-545) <Stefan.Bockes@bmw.de>
2022-04-23 19:56:22 +0200

    Added Dummy Test

---
+++ src/test/java/org/springframework/samples/petclinic/dummy/DummyTest.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.dummy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+	@Test
+	void tooStupidToTest(){
+		assertTrue(true);
+	}
+}
